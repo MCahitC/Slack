dad652809675bf51c3117ce7938431632cedf0ec
MCahitC <64571637+MCahitC@users.noreply.github.com>
2020-05-15 01:44:28 +0100

    Update Login.java

--- src/day_1/Login.java
+++ src/day_1/Login.java
@@ -4,7 +4,11 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Login code from intelliJ"); 
         
-        //this line was added remotely on github
+        //this line was added remotely on github 
+        
+       
+        
+        //simultaneous change made through git hub as intellij
 
     }
 }
