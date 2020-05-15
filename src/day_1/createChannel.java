ab709cee2f7a846cb323a970ad609ae6c1fbe457
MCahitC <64571637+MCahitC@users.noreply.github.com>
2020-05-15 01:55:08 +0100

    Update createChannel.java

--- src/day_1/createChannel.java
+++ src/day_1/createChannel.java
@@ -3,13 +3,13 @@ package day_1;
 public class createChannel {
     public static void main(String[] args) {
         System.out.println("new project");
-
-        int x=0;
-        if(x==0){
+        
+        String s = "a";
+        
+        if(s.contains("a")){
             System.out.println("equal");
-        }else{
+    }else{
             System.out.println("not equal");
-
         }
-    }
+} 
 }
